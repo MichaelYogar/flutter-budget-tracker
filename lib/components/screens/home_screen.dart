c111ccd12cb6e9845d63e4215f7e803e9c8d0a59
Michael Yogar <michaelyogar878@gmail.com>
2022-01-15 13:19:29 -0500

    feat: Added home screen that displays items from Notion API

---
+++ lib/components/screens/home_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_budget_tracker/api/models/item_model.dart';
+import 'package:flutter_budget_tracker/api/view_model/item_viewmodel.dart';
+import 'package:intl/intl.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late ItemViewModel itemViewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    itemViewModel = ItemViewModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {});
+        },
+        child: FutureBuilder<List<ItemModel>>(
+          future: itemViewModel.getItems(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final items = snapshot.data!;
+              return Center(
+                child: Container(
+                  width: 400,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final item = items[index];
+                            return Container(
+                              margin: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[900],
+                                borderRadius: BorderRadius.circular(10.0),
+                                border: Border.all(
+                                  width: 2.0,
+                                  color: Colors.teal,
+                                ),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    offset: Offset(0, 2),
+                                    blurRadius: 6.0,
+                                  ),
+                                ],
+                              ),
+                              child: ListTile(
+                                title: Text(
+                                  item.name,
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                                subtitle: Text(
+                                  '${item.category} • ${DateFormat.yMd().format(item.purchaseDate)}',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                                trailing: Text(
+                                  '-\$${item.price.toStringAsFixed(2)}',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Container(
+                        color: Colors.grey[900],
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const <Widget>[
+                              Text(
+                                'Budget Tracker Notion',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(child: Text('Error'));
+            }
+            // Show a loading spinner.
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
+    );
+  }
+}
