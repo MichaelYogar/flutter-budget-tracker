0e75cad3577221f8d2e72d85f455c6b69907b79d
Michael Yogar <michaelyogar878@gmail.com>
2022-01-15 13:18:57 -0500

    feat: Added item view model

---
+++ lib/api/view_model/item_viewmodel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_budget_tracker/api/api.dart';
+import 'package:flutter_budget_tracker/api/models/item_model.dart';
+import 'package:flutter_budget_tracker/core/core.dart';
+
+class ItemViewModel {
+
+  ItemViewModel();
+
+  Future<List<ItemModel>> getItems() {
+    return app<BudgetService>().getItems();
+  }
+}
